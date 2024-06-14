a50efc24f36c9e4bf0ee0dac631c8f0a1f7a2347
kavindu9611 <kavinduwickramasinghe1920@gmail.com>
2024-06-15 04:22:03 +0530

    Developed the heading section

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Footer from "@/components/footer";
+import Header from "@/components/header";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,7 +19,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${inter.className} text-zinc-900 bg-zinc-100`}>
+        <Header />
         {children}
+        <Footer />
       </body>
     </html>
   );
