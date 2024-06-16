72427ebd93143f8560a02052a4a8209f80f0ecce
kavindu9611 <kavinduwickramasinghe1920@gmail.com>
2024-06-17 03:37:47 +0530

    Resolve a bug by adding  type safety

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -3,11 +3,17 @@ import Link from "next/link";
 export default async function Page() {
   const response = await fetch("https://dummyjson.com/posts?limit=10");
   const data = await response.json();
+
+  type posttype = {
+    id: number;
+    title: string;
+  };
+
   return (
     <main className="text-center pt-16 px-5">
       <h1 className="text-4xl md:text-5xl font-bold mb-5">All posts</h1>
       <ul>
-        {data.posts.map((post) => (
+        {data.posts.map((post: posttype) => (
           <li key={post.id} className="mb-3">
             <Link href={`/posts/${post.id}`}>{post.title}</Link>
           </li>
